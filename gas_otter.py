55d709145804461562e7505e483b386463172a56
Maxwell Wenger <3391859+maxwenger@users.noreply.github.com>
2020-08-21 15:11:20 -0700

    added disclaimer

--- gas_otter.py
+++ gas_otter.py
@@ -12,6 +12,15 @@ print('''
               '._        -'    /
         jgs      ``""--`------`
 
+        *** WARNING AND DISCLAIMER ***
+        THIS PROGRAM IS SHOULD BE USED FOR TRAINING AND EDUCATIONAL PURPOSES
+        ONLY.
+        Blending gasses is dangerous. Not only can you seriously injure or
+        kill yourself while mixing the gasses, but improper blending can lead
+        to serious injury or death of the diver breathing the gas.
+        Do not rely on this program to generate correct or accurate results.
+        Remember to ALWAYS analyze your gas before diving.
+
         Gas Otter v0.0.1 by Maxwell Wenger (GitHub @maxwenger)
 
 ''')
