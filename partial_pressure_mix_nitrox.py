b9b056599683509487f7e188b0468baa4b011458
Maxwell Wenger <3391859+maxwenger@users.noreply.github.com>
2020-08-21 13:39:19 -0700

    Pulled nitrox logic out

--- partial_pressure_mix_nitrox.py
+++ partial_pressure_mix_nitrox.py
@@ -1,17 +1,42 @@
 import sys
 from user_input import UserInput
+from nitrox_calculations import NitroxCalculations
 
-start_mix = UserInput().getFloatInput("Enter the starting mix: ", 0, 1)
-start_pressure = UserInput().getFloatInput("Enter the starting pressure: ", 0, sys.float_info.max)
-desired_mix = start_mix = UserInput().getFloatInput("Enter the desired mix: ", 0, 1)
-desired_pressure = UserInput().getFloatInput("Enter the desired pressure: ", 0, sys.float_info.max)
-fill_mix = start_mix = UserInput().getFloatInput("Enter the fill mix: ", 0, 1)
+while True:
+    print("")
+    start_mix = UserInput().getFloatInput("Enter the starting mix (decimal): ", 0, 1)
+    start_pressure = UserInput().getFloatInput("Enter the starting pressure (psi): ", 0, sys.float_info.max)
+    desired_mix = UserInput().getFloatInput("Enter the desired mix in (decimal): ", 0, 1)
+    desired_pressure = UserInput().getFloatInput("Enter the desired pressure (psi): ", 0, sys.float_info.max)
+    fill_mix = UserInput().getFloatInput("Enter the fill mix (decimal): ", 0, 1)
+    
+    top_off_mix = .21
+    
+    print("")
+    print("Starting with " + str(start_pressure) + " psi of " + str(start_mix*100) + "% nitrox.")
+    print("Trying to make " + str(desired_pressure) + " psi of " + str(desired_mix*100) + "% nitrox.")
+    print("With a fill mix of " + str(fill_mix*100) + "% nitrox")
+    print("With a top-off mix of " + str(top_off_mix*100) + "% nitrox")
+    print("")
+    
+    try:
+        fill_instructions = NitroxCalculations().getFillInstructions(start_pressure, start_mix, desired_pressure, desired_mix, fill_mix, top_off_mix)
+        pressure_in_tank = start_pressure
+        while len(fill_instructions) > 0:
+            fill_instruction = fill_instructions.pop(0)
 
-top_off_mix = .21
+            pressure_in_tank = pressure_in_tank + fill_instruction[0]
 
-fill_pressure = ((desired_pressure * (desired_mix - top_off_mix)) - (start_pressure * (start_mix - top_off_mix))) / (fill_mix - top_off_mix)
-top_off_pressure = desired_pressure - start_pressure - fill_pressure
+            if fill_instruction[0] > 0:
+                print("Fill to " + str(round(pressure_in_tank)) + " psi with " + str(round(fill_instruction[1] * 100, 2)) + "% nitrox. (Δ+" + str(round(fill_instruction[0])) + " psi)")
+            elif fill_instruction[0] < 0:
+                print("Bleed tank to " + str(round(pressure_in_tank)) + " psi (Δ" + str(round(fill_instruction[0])) + " psi)")
+        
+        print("Confirm the final mix is " + str(desired_pressure) + " psi of " + str(round(desired_mix * 100, 2)) + "% nitrox.")
+        print("")
+    except Exception as e:
+        print(e)
 
-print("Add " + str(round(fill_pressure)) + " psi of " + str(fill_mix * 100) + "%.")
-print("Add " + str(round(top_off_pressure)) + " psi of " + str(top_off_mix * 100) + "%.")
+    if input("Would you like to blend another tank of nitrox [Y/n]? ") == 'n':
+        break
 
--- user_input.py
+++ user_input.py
@@ -1,16 +1,18 @@
+import math
+
 class UserInput:
     def getFloatInput(self, message, minValue, maxValue):
         while True:
             try:
                 userInput = float(input(message))
-            except ValueError:
+            except (ValueError, SyntaxError):
                 print("Please input numbers only.")
                 continue
     
             if userInput < minValue:
                 print("Choose a number greater than " + str(minValue))
             elif userInput > maxValue:
-                print("Choose a number less than " + str(minValue))
+                print("Choose a number less than " + str(maxValue))
             else:
                 return userInput
  
